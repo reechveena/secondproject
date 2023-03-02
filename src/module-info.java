69383d319405305e7afd5dc7ad22432a712f17a5
veenasridhar <veenasridhar@localhost>
2023-03-02 15:01:47 -0800

    i am uploading all files to github

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author veenasridhar
+ *
+ */
+module secondproject {
+}
---
+++ src/welcome/welcomejava.java
@@ -0,0 +1,12 @@
+package welcome;
+
+public class welcomejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("welcometo the codelab training session")	
+		
+	}
+
+}
