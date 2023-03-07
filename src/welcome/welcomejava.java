df37d09a853185568851283e3be54208dd9e9fd5
veenasridhar <veenasridhar@localhost>
2023-03-07 15:16:36 -0800

    update the code

--- src/welcome/welcomejava.java
+++ src/welcome/welcomejava.java
@@ -3,10 +3,14 @@ package welcome;
 public class welcomejava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
-		System.out.println("welcometo the codelab training session")	
-		
+		System.out.println("welcome to the java coding class");	
+		int a = 5;
+		int b = 4;
+		int x = a+b;
+		System.out.println(x);
+		System.out.println("testing poll scm");
+		System.out.println("what happened");
 	}
 
 }
